328a9c67f35058758d27d9f88b351f4bbdd6db5b
kushagrasetia <32640056+kushagrasetia@users.noreply.github.com>
2019-11-03 19:41:14 +0530

    gui updated

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords 
 from tkinter import *
 import pickle
+nltk.download('punkt')
+nltk.download('stopwords')
 with open('classifier.pkl','rb') as f:
     loaded_clf=pickle.load(f)
 with open('vectorizer.pkl','rb') as g:
@@ -36,9 +38,22 @@ def sentiment():
     vect=vector(x)
     pred=loaded_clf.predict(vect)
     entry2=Label(top,text=pred).grid(row=2,column=1)
+def open_window():
+    root = Toplevel()
+    root.geometry("600x300+120+120")
+    root.title("Predict the Sentiment")
+    root.config(bg="cyan")
+    t1=StringVar()
+    name=Label(root,text="Name")
+    name.place(x=200,y=100)
+    e=Entry(root,textvariable=t1)
+    e.place(x=238,y=100)
+    button1 = Button(root, text = "Get Sentiment",command = sentiment)
+    button1.place(x=245,y=120)    
 top=Tk()
-t1=StringVar()
-name=Label(top,text="Name").grid(row=0,column=0)
-e=Entry(top,textvariable=t1).grid(row=0,column=1)
-mbutton=Button(top,text='Get Sentiment',command=sentiment).grid(row=1,column=1)
+top.geometry("600x300+120+120")
+top.config(bg ="blue")
+top.title("Top Window")
+mbutton=Button(top,text='Enter review of your choise',command=open_window)
+mbutton.pack()
 top.mainloop()
