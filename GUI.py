9f525ba97d48f8c50d4d0f8975f46bf066e38f53
sandhya kumari <32640148+sandhya1998@users.noreply.github.com>
2019-11-12 12:05:18 +0530

    final tweaks

--- GUI.py
+++ GUI.py
@@ -26,22 +26,22 @@ top.config(bg ="black")
 top.wm_title("ABOUT")
 top.wm_attributes("-fullscreen", "true")
 
-take_UI1 = PhotoImage(file ='images\\Input_Image.png')
+take_UI1 = PhotoImage(file ='Input_Image.png')
 B1=Button(top, image = take_UI1, cursor="hand2", command = open_window)
 B1.pack(anchor=N, padx=500,pady=20,fill='both')
 B1.config(width = 150, height = 150)
 
-bar_plots = PhotoImage(file = 'images\\bar_graph.png')
+bar_plots = PhotoImage(file = 'bar_graph.png')
 B2=Button(top,image =bar_plots,text= 'Plots',compound=LEFT,font=('helv36', 60, 'bold'),command=Plots)
 B2.pack(anchor=SE,padx=300,pady=20,fill='both')
 B2.config(width = 300, height = 300)
 
-acc=PhotoImage(file='images\\features_accuracy.png')
+acc=PhotoImage(file='features_accuracy.png')
 B3=Button(top,image=acc,text='Accuracy',cursor='hand2',command=show_accuracy)
 B3.pack(anchor=S, padx=500,pady=20,fill='both')
 B3.config(width = 150, height = 150)
 
-home_R = PhotoImage(file = 'images\\home.png')
+home_R = PhotoImage(file = 'home.png')
 mbutton=Button(top,image=home_R, font=('helv36', 20, 'bold'),fg = 'white',bg='black', cursor="hand2", command = Return_H)
 mbutton.pack(anchor=SW, padx =500, pady = 20,fill='both')
 mbutton.config(width = 300, height = 120)
@@ -49,7 +49,7 @@ mbutton.config(width = 300, height = 120)
 
 
     
-exit1 = PhotoImage(file = 'images\\Exit_Image.png')
+exit1 = PhotoImage(file = 'Exit_Image.png')
 mbutton1=Button(top,image =exit1,command=exit_window)
 mbutton1.pack(anchor=SE,padx=500,pady=20,fill='both')
 mbutton1.config(width = 300, height = 120)
